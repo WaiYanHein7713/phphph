227b49d725d6497228d70a0a3fb0dc1b1f6297a3
unknown <Kaung Sett Thu>
2019-08-30 14:17:04 +0630

    php

--- web/index.php
+++ web/index.php
@@ -37,7 +37,7 @@ $app->get('/', function() use($app) {
 });
 
 $app->get('/view/', function() use($app) {
-  $st = $app['pdo']->prepare("SELECT StdName FROM students");
+  $st = $app['pdo']->prepare("SELECT * FROM students");
   $st->execute();
 
   $StdName = array();
