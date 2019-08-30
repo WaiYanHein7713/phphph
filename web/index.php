3102b09f965d1624afab17fc615dbf7226bc2e28
unknown <Kaung Sett Thu>
2019-08-30 18:21:29 +0630

    php

--- web/index.php
+++ web/index.php
@@ -2,11 +2,6 @@
 
 require('../vendor/autoload.php');
 
-$GLOBALS['stdName'] = $_GET["name"];
-$GLOBALS['stdAge'] = $_GET["age"];
-$GLOBALS['stdGender'] = $_GET["gender"];
-$GLOBALS['stdPhone'] = $_GET["phone"];
-
 $app = new Silex\Application();
 $app['debug'] = true;
 
