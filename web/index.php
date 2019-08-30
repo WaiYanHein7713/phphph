b183bf4d019c0c584b1140df8a9eacb39e62a3f6
unknown <Kaung Sett Thu>
2019-08-30 12:42:03 +0630

    php

--- web/index.php
+++ web/index.php
@@ -37,7 +37,7 @@ $app->get('/', function() use($app) {
 });
 
 $app->get('/database/', function() use($app) {
-  $st = $app['pdo']->prepare("SELECT * FROM students WHERE StdGender==Male");
+  $st = $app['pdo']->prepare("SELECT * FROM students");
   $st->execute();
 
   $StdName = array();
